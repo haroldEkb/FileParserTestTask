28beb7e4e8947949d8bde70d751a863d48b76897
haroldekb <haroldekb@mail.ru>
2020-08-15 23:17:57 +0500

    Initial commit

---
+++ src/main/java/com/haroldekb/FileTransferTestTask/FileTransferTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.haroldekb.FileTransferTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileTransferTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileTransferTestTaskApplication.class, args);
+	}
+
+}
